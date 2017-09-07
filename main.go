5c9bb77694778293e8d91da9d1fc4b87fdfb1f73
Hongyu <hy2504@columbia.edu>
2017-09-07 13:53:03 -0400

    add EC2 es

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://localhost:9200"
+	ES_URL = "http://54.218.12.95:9200"
 )
 
 func main() {
